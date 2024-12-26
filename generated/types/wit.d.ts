c780d8874a4b3009477fcc40c0f1382b2a465dd1
Alistair Roche <alistair@generalagents.com>
2024-12-27 07:39:58 +0800

    Add files changed by wash dev after source changes

--- generated/types/wit.d.ts
+++ generated/types/wit.d.ts
@@ -3,5 +3,6 @@ import { WasiHttpTypes } from './interfaces/wasi-http-types.js';
 import { WasiIoError } from './interfaces/wasi-io-error.js';
 import { WasiIoPoll } from './interfaces/wasi-io-poll.js';
 import { WasiIoStreams } from './interfaces/wasi-io-streams.js';
+import { WasiLoggingLogging } from './interfaces/wasi-logging-logging.js';
 import { WasiHttpIncomingHandler } from './interfaces/wasi-http-incoming-handler.js';
 export const incomingHandler: typeof WasiHttpIncomingHandler;
